6c234f09fc011728c07b76f9808256cef9a57ec8
Jonah Clark <jonahclark314@gmail.com>
2026-09-20 17:14:53 -0600

    I started Phase 1. I got started on the Make Move tests. I have been able to successfully get pieces to move, and the next step is going to be throwing exceptions if the move is invalid

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -64,7 +64,48 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+        boolean moveIsValid = false;
+        if (this.board.getPiece(move.getStartPosition()) != null) {
+            Collection<ChessMove> moves = validMoves(move.getStartPosition());
+            for (ChessMove possibleMove : moves) {
+                if (possibleMove.equals(move)) {
+                    moveIsValid = true;
+                    break;
+                }
+            }
+        }
+
+        if (this.board.getPiece(move.getStartPosition()).getTeamColor() != getTeamTurn()){
+            moveIsValid = false;
+        }
+
+        if (!moveIsValid) {
+            System.out.println("THROW INVALID MOVE EXCEPTION");
+            throw new InvalidMoveException("This move was not valid!");
+        }
+        else {
+            ChessPiece piece = this.board.getPiece(move.getStartPosition());
+            ChessPiece oldPiece = this.board.getPiece(move.getEndPosition());
+            this.board.removePiece(move.getStartPosition());
+            this.board.removePiece(move.getEndPosition());
+            this.board.addPiece(move.getEndPosition(), piece);
+            boolean resultsInCheck = isInCheck(piece.getTeamColor());
+
+            if (resultsInCheck) {
+                System.out.println("THROW INVALID MOVE EXCEPTION");
+                this.board.removePiece(move.getStartPosition());
+                this.board.removePiece(move.getEndPosition());
+                this.board.addPiece(move.getStartPosition(), piece);
+                this.board.addPiece(move.getEndPosition(), oldPiece);
+                throw new InvalidMoveException("This move was not valid!");
+            } else {
+                if (piece.getTeamColor() == TeamColor.BLACK) {
+                    setTeamTurn(TeamColor.WHITE);
+                } else {
+                    setTeamTurn(TeamColor.BLACK);
+                }
+            }
+        }
     }
 
 
