3d0372b38509503bc07aaa4cdeae64721dfd0d1e
Jonah Clark <jonahclark314@gmail.com>
2026-09-26 10:46:47 -0600

    Finished Phase 1. Updated comments for ChessMove.java

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -28,80 +28,80 @@ public class ChessMove {
         this.PieceType = promotionPiece;
     }
 
+    /**
+     * This checks if the proposed move is a castle move. It does so by seeing if the King moves 2 spaces.
+     * @param board
+     * @return
+     */
     public boolean isACastle (ChessBoard board) {
-        if (board.getPiece(this.startPosition).getPieceType() == ChessPiece.PieceType.KING) {
+        // Make sure the piece moving is a king.
+        if (board.getPiece(this.startPosition) != null && board.getPiece(this.startPosition).getPieceType() == ChessPiece.PieceType.KING) {
             int oldColumn = startPosition.getColumn();
             int newColumn = endPosition.getColumn();
+
+            // Check it moves 2 squares to the left or to the right.
             if (((oldColumn - newColumn) == 2) || ((oldColumn - newColumn) == -2)) {
                 return true;
             } else {
                 return false;
             }
-        } else {
-            return false;
-        }
-    }
-
-    public boolean looksLikeShouldBeEnPassant (ChessBoard board) {
-        int horizontalMovement = Math.abs(this.getStartPosition().getColumn() - this.getEndPosition().getColumn());
-        int verticalMovement = Math.abs(this.getStartPosition().getRow() - this.getEndPosition().getRow());
-        ChessPiece currentPiece = board.getPiece(this.getStartPosition());
-        ChessPiece pieceAtNewLocation = board.getPiece(this.getEndPosition());
-        ChessPosition enPassantPawnPosition = new ChessPosition(this.getStartPosition().getRow(),this.getEndPosition().getColumn());
-        ChessPiece enPassantPawn = board.getPiece(enPassantPawnPosition);
-        return pieceAtNewLocation == null && currentPiece.getPieceType() == ChessPiece.PieceType.PAWN && enPassantPawn != null && enPassantPawn.getPieceType() == ChessPiece.PieceType.PAWN && horizontalMovement == 1 && verticalMovement==1;
-    }
-
-    public boolean isAEnPassant (ChessBoard board, ChessMove lastMove) {
-        System.out.println(" ");
-        System.out.println("Testing En Passant");
-
-        if (lastMove == null) {
-            System.out.println("it was null!");
-            System.out.println(" ");
-            return false;
-        }
-        int newPawnColumnNumber = this.endPosition.getColumn();
-        int oldPawnColumnNumber = this.startPosition.getColumn();
-
-        int lastMoveDistance = lastMove.getStartPosition().getRow() - lastMove.getEndPosition().getRow();
-        int lastMoveColumnNumber = lastMove.getStartPosition().getColumn();
-        ChessPiece enPassantPawn = board.getPiece(lastMove.getEndPosition());
-
-        if (board.getPiece(this.startPosition).getPieceType() == ChessPiece.PieceType.PAWN && board.getPiece(this.endPosition) == null && enPassantPawn.getPieceType() == ChessPiece.PieceType.PAWN && Math.abs(lastMoveDistance) == 2 && newPawnColumnNumber == lastMoveColumnNumber && oldPawnColumnNumber != newPawnColumnNumber ) {
-
-            System.out.println("IS En Passant");
-//            System.out.print("lastMove: ");
-//            System.out.println(lastMove);
-//            System.out.print("Current move: ");
-//            System.out.println(this.toString());
-//            System.out.print("current mover: ");
-//            System.out.println(board.getPiece(this.startPosition));
-//            System.out.print("taking the piece: ");
-//            System.out.println(board.getPiece(lastMove.getEndPosition()));
-//
-//            System.out.println(" ");
-
-            return true;
-        }
-        else {
-
-            System.out.println("is NOT En Passant");
-//            System.out.print("lastMove: ");
-//            System.out.println(lastMove);
-//            System.out.print("Current move: ");
-//            System.out.println(this.toString());
-//            System.out.print("current mover: ");
-//            System.out.println(board.getPiece(this.startPosition));
-//            System.out.print("taking the piece: ");
-//            System.out.println(board.getPiece(lastMove.getEndPosition()));
-//            System.out.println(" ");
-//
+        } else { // if the piece is NOT a king.
             return false;
         }
     }
 
     /**
+     * This is a helper function to see if the move LOOKS like it should be en Passant. Does not take into account
+     * turns.
+     * @param board - current board set up.
+     * @return returns true if the move looks like it should be en Passant. False otherwise.
+     */
+    public boolean looksLikeShouldBeEnPassant (ChessBoard board) {
+        // Make sure it moves 1 square diagonally
+        int horizontalMovement = Math.abs(this.getStartPosition().getColumn() - this.getEndPosition().getColumn());
+        int verticalMovement = Math.abs(this.getStartPosition().getRow() - this.getEndPosition().getRow());
+
+        // See if the current piece is a pawn, make sure there is no piece where it is going.
+        ChessPiece currentPiece = board.getPiece(this.getStartPosition());
+        ChessPiece pieceAtNewLocation = board.getPiece(this.getEndPosition());
+
+        // Make sure that the pawn being taken by the en passant move is actually a pawn.
+        ChessPosition enPassantPawnPosition = new ChessPosition(this.getStartPosition().getRow(),this.getEndPosition().getColumn());
+        ChessPiece enPassantPawn = board.getPiece(enPassantPawnPosition);
+
+        // All of the logic of the comments above is contained in this return statement.
+        return pieceAtNewLocation == null && currentPiece.getPieceType() == ChessPiece.PieceType.PAWN && enPassantPawn != null && enPassantPawn.getPieceType() == ChessPiece.PieceType.PAWN && horizontalMovement == 1 && verticalMovement==1;
+    }
+
+    /**
+     * This tests if the move is ACTUALLY an en passant move. It takes everything into account including what the previous move was.
+     * Compare this to looksLikeShouldBeEnPassant().
+     * @param board - current board setup.
+     * @param lastMove - the most recent move that was done in the game.
+     * @return - returns true if it follows all the en passant rules. returns false otherwise.
+     */
+    public boolean isAEnPassant (ChessBoard board, ChessMove lastMove) {
+
+        // If it is the first move of the game, it is NOT en passant.
+        if (lastMove == null) {
+            return false;
+        }
+
+        // Set up the logic to see if the move resembles an en passant.
+        int newPawnColumnNumber = this.endPosition.getColumn();
+        int oldPawnColumnNumber = this.startPosition.getColumn();
+
+        // Use the last move end location to locate the pawn that potentially can be taken via en passant.
+        int lastMoveDistance = lastMove.getStartPosition().getRow() - lastMove.getEndPosition().getRow();
+        int lastMoveColumnNumber = lastMove.getStartPosition().getColumn();
+        ChessPiece enPassantPawn = board.getPiece(lastMove.getEndPosition());
+
+        // The return implements all of the logic stated above.
+        return board.getPiece(this.startPosition).getPieceType() == ChessPiece.PieceType.PAWN && board.getPiece(this.endPosition) == null && enPassantPawn.getPieceType() == ChessPiece.PieceType.PAWN && Math.abs(lastMoveDistance) == 2 && newPawnColumnNumber == lastMoveColumnNumber && oldPawnColumnNumber != newPawnColumnNumber;
+    }
+
+    /**
+     * This is what returns the start position of the move.
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
@@ -109,6 +109,7 @@ public class ChessMove {
     }
 
     /**
+     * This is what returns the end position of the move.
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
@@ -150,6 +151,10 @@ public class ChessMove {
         return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && PieceType == chessMove.PieceType;
     }
 
+    /**
+     * This is a override of the hashCode method. It helps with the equals method above.
+     * @return the hash code. It is an int.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(startPosition, endPosition, PieceType);
